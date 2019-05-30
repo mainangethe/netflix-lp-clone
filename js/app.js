ea0d068057b8e2c0d01d892db9d1f326890e1fa5
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-05-30 08:38:43 +0300

    Add dynamic tab content selection
    - on tab selection; it should also show the respective tab's content

--- js/app.js
+++ js/app.js
@@ -5,9 +5,16 @@ tabItems.forEach(item => item.addEventListener('click', selectItem));
 
 function selectItem(event) {
   removeBorder();
+  removeShow();
   this.classList.add('tab-border');
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add('show');
 }
 
 function removeBorder() {
   tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow(){
+  tabContentItems.forEach(item => item.classList.remove('show'));
 }
